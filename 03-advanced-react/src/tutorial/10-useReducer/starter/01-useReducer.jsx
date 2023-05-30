5ba818e04e7d49026ce3ad11f0f24c26ac05bff4
Emmacodec <danielemmanuel7553@gmail.com>
2023-05-30 05:26:39 +0100

    update on useReducer

--- 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,15 +1,50 @@
-import React from 'react';
-import { data } from '../../../data';
-const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+import {useState, useReducer} from 'react';
+import { data, people } from '../../../data';
 
-  const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
-  };
-  return (
+const defaultState = {
+  people: data,
+  isLoading: false,
+};
+
+const CLEAR_LIST = 'CLEAR_LIST'
+const RESET_LIST = 'RESET_LIST'
+const REMOVE_ITEM = 'REMOVE_ITEM'
+
+// the empty function reducer controls our entire state
+const reducer = (state, action) => {
+  if(action.type === CLEAR_LIST) {
+    return {...state, people: [] };
+  }
+  if(action.type === RESET_LIST) {
+    return {...state, people: data};
+  }
+  if(action.type === REMOVE_ITEM) {
+    let newPeople = state.people.filter((person) => person.id !== action.payload.id);
+
+    return {...state, people: newPeople};
+  }
+  throw new Error('No matching' `${action.type}`);
+};
+
+const ReducerBasics = () => {
+const [state,dispatch] = useReducer(reducer, defaultState);
+
+const removeItem = (id) => {
+  dispatch({type: REMOVE_ITEM, payload: { id }});
+    };
+
+const clearList = () => {
+  dispatch({type: CLEAR_LIST})
+};
+
+const resetList = () => {
+  dispatch({type: RESET_LIST})
+};
+console.log(state);
+
+return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className='item'>
@@ -18,13 +53,24 @@ const ReducerBasics = () => {
           </div>
         );
       })}
+      {state.people.length < 1 ? (
       <button
         className='btn'
         style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
+        onClick={resetList}
+      >
+        reset items
+      </button>
+      ) : (
+        <button
+        className='btn'
+        style={{ marginTop: '2rem' }}
+        onClick={clearList}
       >
         clear items
       </button>
+      )};
+      
     </div>
   );
 };

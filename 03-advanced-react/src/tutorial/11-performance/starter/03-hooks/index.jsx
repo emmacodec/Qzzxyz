2ec59df7d9fdbbb307f8d15a91a0596c5d14b642
Emmacodec <danielemmanuel7553@gmail.com>
2023-06-01 09:33:43 +0100

    update on useMemo,useTransition hooks in react

--- 03-advanced-react/src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ 03-advanced-react/src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 import { data } from '../../../../data';
 import List from './List';
+import SlowFunction from './SlowFunction';
 const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
 
-  const removePerson = (id) => {
+  const value = useMemo(() => SlowFunction(),[]);
+  
+  console.log(value);
+
+  const removePerson = useCallback((id) => {
+    console.log(people);
     const newPeople = people.filter((person) => person.id !== id);
     setPeople(newPeople);
-  };
-
+  }, 
+  [people]
+  ); 
+  
   return (
     <section>
       <button
